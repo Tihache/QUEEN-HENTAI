47699a53554aaac5e55ce8a1b3de76cc7bcad745
Tihache <152892312+Tihache@users.noreply.github.com>
2023-12-05 04:38:48 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+global.owner = [TiDollar
+  ['263715042697', 'Dinuwa Official²⁰²³', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
